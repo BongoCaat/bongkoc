9aa1fd0488db85365e2c7af3da43448d1cf86ada
BongoCaat <102444425+BongoCaat@users.noreply.github.com>
2022-12-10 20:10:28 -0300

    Add files via upload

---
+++ load.lua
@@ -0,0 +1,7 @@
+wait(1)
+
+if game.PlaceId == 1537690962 then
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/BongoCaat/bongkoc/main/bongkoc.lua"))()
+else
+	warn("Game Not Supported!!!")
+end
